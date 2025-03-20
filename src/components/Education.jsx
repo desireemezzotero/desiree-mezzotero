e831127bee58204d4370907585c887eee7fa77fe
desireemezzotero <desireemezzotero1@gmail.com>
2025-03-20 18:48:34 +0100

    add education

---
+++ src/components/Education.jsx
@@ -0,0 +1,38 @@
+function Education() {
+  return (
+    <>
+      <div className="container border-t-2 border-neutral-900 mx-auto max-w-screen-xl w-52 mt-8">
+      </div>
+      <div className="max-w-screen-xl container mx-auto p-6">
+        <h2 className="mb-6 text-6xl text-neutral-500 dancing-script">Education</h2>
+
+        <div className="text-white flex">
+          <i class="fa-solid fa-code text-white text-2xl mr-3"></i>
+
+          <p className="font-bold mr-2 ">
+            Web Development with Boolean:
+          </p>
+          <p>
+            Master Full-Stack Web Development
+          </p>
+
+        </div>
+
+        <div className="text-white flex  mt-4">
+          <i class="fa-solid fa-graduation-cap text-white text-2xl mr-3"></i>
+
+          <p className="font-bold mr-2">
+            Technical and Vocational Diploma:
+          </p>
+          <p>
+            Administration, Finance and Marketing Art. International Relations for Marketing
+          </p>
+        </div>
+
+
+      </div>
+    </>
+  )
+}
+
+export default Education
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
 import InitialPart from "../components/InitialPart"
 import About from "../components/About"
 import Services from "../components/Services"
+import Education from "../components/Education"
+import Project from "../components/Project"
+
 function HomePage() {
   return (
     <>
       <InitialPart />
       <About />
       <Services />
+      <Education />
+      <Project />
     </>
   )
 }

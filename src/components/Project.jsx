0cd260ac23a5ab88827299df13fc117109de807b
desireemezzotero <desireemezzotero1@gmail.com>
2025-07-21 11:28:56 +0200

    refactor Project component layout and update HomePage structure

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,91 +1,31 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay'
-
-import { Autoplay, EffectCoverflow, Pagination } from 'swiper/modules';
-
-
+import json from "../data/data.json"
 
 function Project() {
   return (
     <>
-      <div className="container border-t-2 border-neutral-900 mx-auto max-w-screen-xl w-52 mt-8">
+      {/* linea */}
+      <div className="container border-t-2 border-neutral-900 mx-auto max-w-screen-xl w-52 mt-5">
       </div>
+
+      {/* titolo */}
       <div className="max-w-screen-xl container mx-auto p-6">
-        <h2 className="mb-6 text-6xl text-neutral-500 dancing-script">Project with Boolean</h2>
+        <h2 className="mt-10 mb-6 text-5xl font-semibold text-black text-center">
+          Progetti
+        </h2>
 
-        <p className="text-white">
-          Thanks to the course, I acquired a number of skills and during the six months of training I worked on numerous projects, some of them in collaboration with a team. Below is a selection of my work: click on each project to view the details.
-        </p>
+        {/* card */}
+        <div className="grid-cols-2 md:grid-cols-4 gap-3 grid">
 
-        <Swiper
-          effect={'coverflow'}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={3}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          autoplay={{
-            delay: 1000,
-            disableOnInteraction: false,
-          }}
-          modules={[EffectCoverflow, Pagination, Autoplay]}
-          className="mySwiper mt-7"
-        >
+          {
+            json.map((json) =>
+              <div key={json.id} className="block max-w-sm p-6 bg-dark border border-gray-200 rounded-lg shadow-sm bg-black hover:scale-110 duration-300">
+                <h5 className="mb-2 tracking-tight text-gray-900 dark:text-white">{json.title}</h5>
+              </div>)
+          }
 
-          <SwiperSlide className='border border-neutral-400 justify-items-center justify-center p-3'>
-            <h2 className="mb-6 text-3xl text-neutral-500 dancing-script">Doctor</h2>
-            <p>Development of complete web solutions ranging from user interface (UI) design to backend logic management</p>
-          </SwiperSlide>
 
-          <SwiperSlide className='border-2 border-neutral-400 justify-items-center justify-center p-3'>
-            <h2 className="mb-6 text-3xl text-neutral-500 dancing-script">Front-End Development</h2>
-            <p>I create intuitive and engaging user experiences, with responsive designs that fit any device. I use modern technologies like React, Bootstrap, and Tailwind CSS to build elegant and functional interfaces.</p>
-          </SwiperSlide>
-
-          <SwiperSlide className='border-2 border-neutral-400 justify-items-center justify-center p-3'>
-            <h2 className="mb-6 text-3xl text-neutral-500 dancing-script">Back-end Development</h2>
-            <p>Build secure and scalable systems for the server side of your website. I use technologies like Node.js, Express.js and PHP to ensure optimal performance and efficient data management.</p>
-          </SwiperSlide>
-
-          <SwiperSlide className='border-2 border-neutral-400 justify-items-center justify-center p-3'>
-            <h2 className="mb-6 text-3xl text-neutral-500 dancing-script">Design with Figma</h2>
-            <p>Design interactive layouts and prototypes with Figma, turning ideas into visually appealing and easily navigable designs optimized for the user experience.</p>
-          </SwiperSlide>
-
-          <SwiperSlide className='border-2 border-neutral-400 justify-items-center justify-center p-3'>
-            <h2 className="mb-6 text-3xl text-neutral-500 dancing-script">Poster Design</h2>
-            <p>I offer graphic design services for the creation of posters and promotional materials, with a focus on visibility and visual impact for your brand or event.</p>
-          </SwiperSlide>
-
-          <SwiperSlide className='border-2 border-neutral-400 justify-items-center justify-center p-3'>
-            <h2 className="mb-6 text-3xl text-neutral-500 dancing-script">Web Page Management</h2>
-            <p>I manage and continuously update your website and social media, ensuring that they are always up to date, performing and relevant</p>
-          </SwiperSlide>
-        </Swiper>
+        </div>
       </div>
-
-
-      <div className="text-white flex  mt-4">
-        <i class="fa-solid fa-graduation-cap text-white text-2xl mr-3"></i>
-
-        <p className="font-bold mr-2">
-          Technical and Vocational Diploma:
-        </p>
-        <p>
-          Administration, Finance and Marketing Art. International Relations for Marketing
-        </p>
-      </div>
-
-
     </>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,6 +9,7 @@ function HomePage() {
     <>
       <InitialPart />
       <About />
+      <Project />
       <Services />
       <Education />
     </>
